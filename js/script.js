221fe28ff22f876c84688450814e909acbc66444
Anton <bur2000_2010@mail.ru>
2021-03-16 11:33:07 +0400

    основы

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// let options={
+//     width: 1366,
+//     height: 768,
+//     background: 'red',
+//     font: {
+//         size:'16px',
+//         color: 'white'
+//     }
+// };
+
+// console.log(JSON.stringify(options));//перевод объекта в JSON(На сервер)
+
+// console.log(JSON.parse(JSON.stringify(options)));//Принимать и преобразовывать данные с сервера
+
+let inputRub = document.getElementById('rub'),
+    inputUsd=document.getElementById('usd');
